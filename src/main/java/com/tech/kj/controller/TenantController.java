ac89f3cc48042cafcd1d0e56b533d6168d2ccf3a
Kishan <kishanjvns@gmail.com>
2024-10-19 12:18:21 +0530

    Update TenantController.java
    correct import

--- src/main/java/com/tech/kj/controller/TenantController.java
+++ src/main/java/com/tech/kj/controller/TenantController.java
@@ -4,6 +4,10 @@ import com.tech.kj.config.TenantContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("tenant")
